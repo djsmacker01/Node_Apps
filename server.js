5570d559c022d80f657fa1c2b4acc48630acc518
Nurudeen Adedeji <djsmacker01@gmail.com>
2025-01-08 14:54:05 +0000

    commit

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const http = require('http')
+const express = require('express')
 
-const server = http.createServer((req, res) => { 
-    console.log(req)
-})
 
 server.listen(3000, () => {
     console.log('Server is running on port 3000')
