4d09945e2519bd057819a41d5f92051bceb9f087
Nurudeen Adedeji <djsmacker01@gmail.com>
2025-01-22 20:25:40 +0000

    commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express')
 
 const app = express(); 
-
+const PORT = 3000
 app.use((req,res, next) => {
     console.log('In the middleware')
     next()
@@ -13,7 +13,7 @@ app.use((req,res,next) => {
     res.send('<h1>Hello from express</h1>')
 })
 
-
+app.listen(port)
 // server.listen(3000, () => {
 //     console.log('Server is running on port 3000')
 // })
