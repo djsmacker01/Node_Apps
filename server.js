85656685c54413db9f283f6493d2f387e50e1a16
Nurudeen Adedeji <djsmacker01@gmail.com>
2025-01-22 20:22:53 +0000

    commit

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ app.use((req,res,next) => {
 })
 
 
-const server = http.createServer(app);
-server.listen(3000, () => {
-    console.log('Server is running on port 3000')
-})
+// server.listen(3000, () => {
+//     console.log('Server is running on port 3000')
+// })
