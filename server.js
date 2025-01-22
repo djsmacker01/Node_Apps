df0e1a89829a5985eeb8b1ee3b429a044519327f
Nurudeen Adedeji <djsmacker01@gmail.com>
2025-01-22 21:00:04 +0000

    commit

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const express = require('express')
 
 const app = express(); 
 // const PORT = 3000
-app.use((req,res, next) => {
-    console.log('In the middleware')
-    next()
+// app.use((req,res, next) => {
+//     console.log('In the middleware')
+//     next()
+// })
+app.use('/add-product', (req, res, next) => {
+    console.log('Add Product')
+    res.send('<h1> Add product page<h1/>')
 })
 
-app.use((req,res,next) => {
+app.use('/',(req,res) => {
     console.log('Another the middleware')
     res.send('<h1>Hello from express</h1>')
 })
