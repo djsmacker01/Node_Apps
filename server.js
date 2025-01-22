56b16389c9a1454397c0ee9a25987ebc671174fa
Nurudeen Adedeji <djsmacker01@gmail.com>
2025-01-22 21:21:13 +0000

    commit

--- server.js
+++ server.js
@@ -9,12 +9,14 @@ const app = express();
 // })
 app.use('/add-product', (req, res, next) => {
     console.log('Add Product')
-    res.send('<h1> Add product page<h1/>')
+    // res.send('<h1> Add product page<h1/>')
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit"><button/></form > ')
+
 })
 
-app.use('/',(req,res) => {
+app.use('/product',(req,res) => {
     console.log('Another the middleware')
-    res.send('<h1>Hello from express</h1>')
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit"><button/></form > ')
 })
 
 
