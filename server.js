d539d9493b41f6e70f23a35b8560da18723d7dfc
Nurudeen Adedeji <djsmacker01@gmail.com>
2025-01-23 15:02:41 +0000

    commit

--- server.js
+++ server.js
@@ -1,22 +1,14 @@
 const express = require('express');
+const adminRoutes = require('./routes/admin');
+const shopRouter = require('./routes/shop');
 const bodyParser = require('body-parser');
 const app = express();
 
 // Middleware
-app.use(bodyParser.urlencoded({extended: false}))
-app.get('/',(req, res)=> {
-    console.log('Hello from the root route');
-    res.send('Hello from the root route');
-    
-})
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get('/about',(req, res)=> {
-    console.log('Hello from the about route');
-    console.log(req.body)
-    res.send('Hello from the about route');
-})
-
-
+app.use(adminRoutes);
+app.use(shopRouter);
 
 app.listen(3000, ()=> {
     console.log('Server is running on port 3000');
