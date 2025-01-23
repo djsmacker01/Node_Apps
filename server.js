ce51cd703365b4a85e80b4f894dbb1b0e6e7991c
Nurudeen Adedeji <djsmacker01@gmail.com>
2025-01-23 13:50:46 +0000

    commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 // Middleware
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: false}))
 app.get('/',(req, res)=> {
     console.log('Hello from the root route');
     res.send('Hello from the root route');
@@ -15,6 +15,8 @@ app.get('/about',(req, res)=> {
     console.log(req.body)
     res.send('Hello from the about route');
 })
+
+
 
 app.listen(3000, ()=> {
     console.log('Server is running on port 3000');
