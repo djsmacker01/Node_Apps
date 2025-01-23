22614ab7da1b4b34d2b36f709f4f5387aa5520d6
Nurudeen Adedeji <djsmacker01@gmail.com>
2025-01-23 15:13:53 +0000

    commit

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(adminRoutes);
 app.use(shopRouter);
 
+
 app.listen(3000, ()=> {
     console.log('Server is running on port 3000');
 })
