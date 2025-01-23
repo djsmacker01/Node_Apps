3e0a4df14ed2689fa8fdcf3ff11ed7886ed05bb3
Nurudeen Adedeji <djsmacker01@gmail.com>
2025-01-23 13:29:04 +0000

    commit

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 
 // Middleware
-app.use('/',(req, res)=> {
+app.get('/',(req, res)=> {
     console.log('Hello from the root route');
     res.send('Hello from the root route');
     
 })
 
-app.use('/about',(req, res)=> {
+app.get('/about',(req, res)=> {
     console.log('Hello from the about route');
     res.send('Hello from the about route');
 })
