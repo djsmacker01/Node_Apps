dddd12fe7343542e46d61b232333168a5433dd2d
Nurudeen Adedeji <djsmacker01@gmail.com>
2025-01-24 18:12:45 +0000

    commit

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,6 @@
 const express = require('express')
 
-// const router = express.Router()
+const router = express.Router()
 
 router.get('/add-product', (req, res, next) => {
     res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
