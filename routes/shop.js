2bbf9206705fbd5a311c653fe0d59c6ff9ff55e6
Nurudeen Adedeji <djsmacker01@gmail.com>
2025-01-29 12:57:53 +0000

    commit

--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,11 @@
 const express = require('express')
+const path = require('path')
 
 const router = express.Router()
 
 router.get('/',(req, res)=> {
     console.log('Shope page');
-    res.sendFile('/');
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
     
 })
 
