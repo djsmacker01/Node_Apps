1b9cec75e9065839781b3f824f88a529241a9f6e
Nurudeen Adedeji <djsmacker01@gmail.com>
2025-01-29 10:10:32 +0000

    commit

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ const shopRouter = require('./routes/shop');
 const bodyParser = require('body-parser');
 const app = express();
 
+app.use(adminRoutes);
+app.use(shopRouter);
+
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use(adminRoutes);
-app.use(shopRouter);
 
 
 app.listen(3000, ()=> {
