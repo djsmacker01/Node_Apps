96fdca725d5346307005c5e510f3172032b6dec6
Nurudeen Adedeji <djsmacker01@gmail.com>
2025-02-08 11:06:20 +0000

    commit

--- routes/admin.js
+++ routes/admin.js
@@ -3,10 +3,12 @@ const path = require('path')
 const rootDir = require('../util/path')
 const router = express.Router()
 
+// GET request for admin/add-product page
 router.get('/admin/add-product', (req, res, next) => {
     res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
 })
 
+// POST request for admin/add-product page
 router.post('/admin/add-product', (req,res,next) => {
     console.log(req.body)
     res.redirect('/')
