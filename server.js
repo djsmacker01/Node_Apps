c781e88aca24bba1ed2be2387439b84489a5b708
Nurudeen Adedeji <djsmacker01@gmail.com>
2025-02-09 21:13:25 +0000

    commit

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(adminData.routes);
 app.use(shopRouter); 
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname,'views','404.html'))
+    res.status(404).render('404')
 })
 
 
